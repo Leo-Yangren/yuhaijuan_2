1d0cc4df3598f42d4f4cb4afc614ff2b70020599
Youngleo <450486294@qq.com>
2016-04-10 23:08:02 +0800

    Initial Commit

---
+++ Haijuan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Haijuan
+//
+//  Created by 杨鑫勇 on 16/4/10.
+//  Copyright © 2016年 杨鑫勇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
